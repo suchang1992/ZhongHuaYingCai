a411fd7f1a5a532c7ccd2105ef122ea95bed198c
suchang1992 <549917764@qq.com>
2015-06-10 16:21:23 +0800

    参数类

---
+++ src/main/java/Utils/CommonParameter.java
@@ -0,0 +1,11 @@
+package Utils;
+
+/**
+ * Created by Administrator on 2015/6/10.
+ */
+public class CommonParameter {
+    //简历详情页
+    public static final String resume_detil_url = "http://www.chinahr.com/modules/jmw/SocketAjax.php?m=hmresume&f=getresume&tp=4";
+    //简历列表页
+    public static final String resume_list_url = "http://www.chinahr.com/modules/jmw/SocketAjax.php?m=hmresume&f=resume&action=myresume&list_type=search&usetoken=1";
+}
